e01eba9913b1f22d8ec567c50b000f6f4a3194e2
Tommy-Lim <tlim24@gmail.com>
2016-12-23 14:59:44 -0800

    added checkbox function and fixed scrolling for nav including toggles at top

--- index.js
+++ index.js
@@ -21,54 +21,83 @@ app.use(express.static(path.join(__dirname, 'static'))); //boilerplate
 
 // ROUTES
 
+// home page
 app.get('/', function(req,res){
   res.render('site/home');
 });
 
+//about page
 app.get('/about', function(req,res){
   res.render('site/about');
 });
 
+// account page
 app.get('/account', function(req,res){
   res.render('site/account');
 });
 
+// cart page
 app.get('/cart', function(req,res){
   res.render('site/cart');
 });
 
+// contact page
 app.get('/contact', function(req,res){
   res.render('site/contact');
 });
 
+// search function with checkboxes
 app.get('/search', function(req,res){
-  console.log(req.query);
-  console.log(req.query.title);
+
+  // setup submission output and query string
   query = req.query;
   q = query.search;
+
+  // check if the box is equal to true and if so, does the value include the query
+  function isMatch(checkValue, value, query){
+    value = value.toLowerCase();
+    query = query.toLowerCase();
+    return checkValue === 'true' && value.includes(query);
+  }
+
+  //
   db.product.findAll().then(function(products){
     var results = [];
+
     products.forEach(function(product){
 
-      title = product.title.toLowerCase();
-      details = product.details.toLowerCase();
-      location = product.location.toLowerCase();
-      make = product.make.toLowerCase();
-      model = product.model.toLowerCase();
-      ownerID = product.ownerID.toLowerCase();
+      title = product.title;
+      details = product.details;
+      location = product.location;
+      make = product.make;
+      model = product.model;
+      ownerID = product.ownerID;
 
-      if(title.indexOf(q)>=0||details.indexOf(q)>=0||location.indexOf(q)>=0||make.indexOf(q)>=0||model.indexOf(q)>=0||ownerID.indexOf(q)>=0){
-        results.push(product);
-      }
-
+      if (isMatch(query.title, title, q) ||
+          isMatch(query.details, details, q) ||
+          isMatch(query.location, location, q) ||
+          isMatch(query.make, make, q) ||
+          isMatch(query.model, model, q) ||
+          isMatch(query.ownerID, ownerID, q)){
+            results.push(product);
+          }
     });
-    res.render('post/search-results',{products:results, query: query});
+    res.render('post/index',{
+      products:results,
+      query: query,
+      showOptions: true,
+      page: 'search'
+    });
   });
 });
 
 app.get('/post/all', function(req,res){
   db.product.findAll().then(function(products){
-    res.render('post/index',{products:products});
+    res.render('post/index',{
+      products:products,
+      showOptions: true,
+      page: 'all'
+    });
   });
 });
 
